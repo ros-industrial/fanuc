07df6911ac510cbc604ab8bc812837d67a33c4c5
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2020-02-27 22:33:25 +0000

    added m6ib/6s moveit plugin with ikfast

---
+++ fanuc_m6ib6s_moveit_plugins/update_ikfast_plugin.sh
@@ -0,0 +1,1 @@
+rosrun moveit_kinematics create_ikfast_moveit_plugin.py --search_mode=OPTIMIZE_MAX_JOINT --srdf_filename=fanuc_m6ib6s.srdf --robot_name_in_srdf=fanuc_m6ib6s --moveit_config_pkg=fanuc_m6ib6s_moveit_config fanuc_m6ib6s manipulator fanuc_m6ib6s_moveit_plugins base_link tool0 /home/joao/catkin_ws/fanuc_m6ib6s_moveit_plugins/src/fanuc_m6ib6s_manipulator_ikfast_solver.cpp
